84b6393077fab80bb5e4090c8b667947d5edadd6
Zelhorst92 <r.zelhorst@gmail.com>
2021-05-07 12:53:21 +0000

    Added hide function to navbar on click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,10 @@
         /* Javascript snippets supplied by my mentor, Simen Daehlin */
 
+        /* Navigation */
+        $('.navbar-nav>li>a').on('click', function () {
+            $('.navbar-collapse').collapse('hide');
+        });
+
         /* Sign-up Success Modal */
         $('#sign-up-form').on('submit', function (e) {
             e.preventDefault();
